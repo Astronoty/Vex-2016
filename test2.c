f3ed70732243485e9409c02192b40375851bba8a
wowei12 <wowei12@users.noreply.github.com>
2016-09-14 15:52:28 -0500

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,36 +1,35 @@
-#define JOYSTICK_DEAD_ZONE 10;
-
-void driveCTL();
-void driveSYS();
 
 
 
 
-
-
-void drivesys
+void drivesys()
 {
-		int verticalInput, horizontalInput;
+	//	int verticalInput, horizontalInput;
+		int JOYSTICK_DEAD_ZONE = 10;
+		int verticalInput = vexRT[Ch3];
+		int horizontalInput = vexRT[Ch1];
 
-		verticalInput = vexRT[Ch3];
-		horizontalInput = vexRT[Ch1];
-
-
-#ifdef JOYSTICK_DEAD_ZONE
-		if (abs(verticalinput) > JOYSTICK_DEAD_ZONE || abs(horizontalInput) > JOYSTICK_DEAD_ZONE)
+		if(abs(horizontalInput) > JOYSTICK_DEAD_ZONE) || (abs(verticalInput) > JOYSTICK_DEAD_ZONE)
 		{
-
+			int meme = 0;
+			int y = verticalInput;
+			int x = horizontalInput;
+			
+			motor[port1] = y; //placeholder motor; replace y with PID request
+			motor[port2] = y;  //"
+			
 		}
-#endif
-
 }
 
-
+void drivesysPID()
+{
+	int meme = 0;
+}
 
 
 task main()
 {
-
+	drivesys();
 
 
 }
