4510b90e97428af435f645a33e113663483f7c2c
wowei12 <wowei12@users.noreply.github.com>
2016-10-12 14:58:57 -0500

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,4 +1,6 @@
-
+int verticalInput = vexRT[Ch3];
+int horizontalInput = vexRT[Ch1];
+int slowTurn = vexRT[Btn8L]
 
 
 
@@ -6,18 +8,23 @@ void drivesys()
 {
 	//	int verticalInput, horizontalInput;
 		int JOYSTICK_DEAD_ZONE = 10;
-		int verticalInput = vexRT[Ch3];
-		int horizontalInput = vexRT[Ch1];
 
 		if(abs(horizontalInput) > JOYSTICK_DEAD_ZONE) || (abs(verticalInput) > JOYSTICK_DEAD_ZONE)
 		{
-			int meme = 0;
-			int y = verticalInput;
-			int x = horizontalInput;
+			if(slowTurn == 0)
+			{
+				int y = verticalInput;
+				int x = horizontalInput;
+			}
 			
-			motor[port1] = y; //placeholder motor; replace y with PID request
-			motor[port2] = y;  //"
+			else if(slowTurn == 1)
+			{
+				int y = verticalInput /2;
+				int x = horizontalInput /2;
+			}
 			
+			motor[port1] = y; 
+			motor[port2] = y;			
 		}
 }
 
